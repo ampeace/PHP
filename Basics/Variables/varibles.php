870e7f124795e2c27a6f4f618392cb3e586c1cde
ampeace <aman.kumar.singh.541@gmail.com>
2026-04-16 00:34:11 +0530

    variables

---
+++ Basics/Variables/varibles.php
@@ -0,0 +1,18 @@
+<?php
+// Variables in PHP start with $ sign
+
+$name = "Rahul";   // string variable
+$age = 20;         // integer variable
+$price = 99.99;    // float variable
+
+// Display variables
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+
+echo "<br>";
+
+// Rules:
+// 1. Must start with $
+// 2. Cannot start with number
+// 3. Case-sensitive ($name and $Name are different)
+?>
