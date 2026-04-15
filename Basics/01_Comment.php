0a84ee124c9df6669eb7b53da347464bcba82874
ampeace <aman.kumar.singh.541@gmail.com>
2026-04-16 00:32:10 +0530

    How to comment

---
+++ Basics/01_Comment.php
@@ -0,0 +1,40 @@
+<?php
+// This is a SINGLE-LINE comment using //
+
+// You can also use # for single-line comment
+# This is another single-line comment
+
+echo "Hello World"; // Comment at end of line
+
+echo "<br>";
+
+/*
+This is a MULTI-LINE comment
+It can span multiple lines
+Useful for explaining large blocks of code
+*/
+echo "PHP Comments Example";
+
+echo "<br>";
+
+// Example: using comments in logic
+$a = 10;
+$b = 20;
+
+// Add two numbers
+$sum = $a + $b;
+
+echo "Sum = " . $sum;
+
+echo "<br>";
+
+/*
+Multi-line comment can also be used
+to temporarily disable code (debugging purpose)
+
+echo "This line will not run";
+echo "Because it is inside comment";
+*/
+
+echo "Code continues here";
+?>
